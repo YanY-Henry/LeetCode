81c08477c1637ddcf28c0e533411128d7bce631e
Henry Yuhang YAN <96647290+YanY-Henry@users.noreply.github.com>
2024-04-29 02:14:01 +0200

    Update gitpush.sh

--- gitpush.sh
+++ gitpush.sh
@@ -38,8 +38,36 @@ check_difficulty "$difficulty"
 current_date=$(date +%Y.%m.%d)
 
 
-# update README.md
-echo -e "\n| $num | $title | [Code](https://github.com/YanY-Henry/LeetCode/blob/main/code/$num.py) | $difficulty |" >> README.md
+# read existing solution information
+existing_solutions=$(sed -n '/^\| #\|^---/p' README.md)
+
+
+# extract solution information from existing solutions
+while read -r line; do
+    if [[ $line =~ ^\| [0-9]{4} \| ]]; then
+        num=$(echo $line | cut -d '|' -f 2)
+        title=$(echo $line | cut -d '|' -f 3 | sed 's/^"|"$//g')
+        code_link=$(echo $line | cut -d '|' -f 4 | sed 's/^"|"$//g')
+        difficulty=$(echo $line | cut -d '|' -f 5 | sed 's/^"|"$//g')
+        solutions[$num]="|$num|$title|$code_link|$difficulty|"
+    fi
+done <<< "$existing_solutions"
+
+
+# add new solution information
+new_solution="|$num|$title|[Code](https://github.com/YanY-Henry/LeetCode/blob/main/code/$num.py)|$difficulty|"
+solutions[$num]=$new_solution
+
+
+# sort solutions array by num
+sorted_solutions=$(sort -k 1n <<< "${!solutions[@]}")
+
+
+# generate new table
+echo "| # | Title | Solution | Difficulty |" | tee README.md
+for num in "${sorted_solutions[@]}"; do
+    echo "${solutions[$num]}" >> README.md
+done
 
 
 # update GitHub repo
